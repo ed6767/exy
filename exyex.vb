84ad72e5323359b0b0cc81d4222a6190c22d6005
Ed.E <edxtdev@gmail.com>
2019-08-20 21:49:40 +0100

    Add variables, error handling

--- exyex.vb
+++ exyex.vb
@@ -1,14 +1,27 @@
-﻿Public Class exyex
+Public Class exyex
+    ' EXY (c) 2019 Ed.E
+    Public Shared exyVersion As Short = 1
 
-    Public Shared Event InitRun() 'Called before runtime
-    Public Shared Event FunctionCall(ByVal args As List(Of String))
     ' Instruct, dictionary of instructions to run
     ' Instructionindex = List of string(0)Instruction, (1)(2)(3...) arguments, varibles can be replaced on the fly in mode
     Public Shared instruct As Dictionary(Of Integer, List(Of String)) = New Dictionary(Of Integer, List(Of String))
     Shared instructIndex As Integer = 0
     ' Goto label, closest instruction index
     Public Shared gotoLocations As Dictionary(Of String, Integer) = New Dictionary(Of String, Integer)
-    Public Shared runtimeTemp = ""
+    Public Shared runtimeTemp As Object = "Not defined"
+    Public Shared runningCode As Boolean = False
+    Public Shared runningErrorHandling As Dictionary(Of String, Object) = New Dictionary(Of String, Object) From
+     {
+        {"enabled", False},
+        {"evenFatal", False}, ' Hand fatal errors to the script
+        {"gotoMarker", ""},
+        {"warnings", True}
+     }
+
+    Public Shared varibles As Dictionary(Of String, Object) = New Dictionary(Of String, Object) From
+        {
+        {"_VERSION", exyVersion}
+        }
 
     'Load code handlers
     Shared Sub loadHandlers()
@@ -20,15 +33,17 @@
         exyconst.functionList = New Dictionary(Of String, Byte) From
     {
     {"", 0}, {" ", 0}, 'nothing
-    {"on error", 1}, {"die", 1}, 'set error action
+    {"on error", 1}, {"die", 1}, {"warnings", 1}, 'set error action
     {"if", 2}, 'If temp bool is true goto arg 1 else goto arg 2 (arg 2 can be continue which does nothing)
-    {"goto", 1} 'jump to marker
+    {"goto", 1}, 'jump to marker
+    {"var", 1}, ' define var
+    {"set", 2} ' Set var
     }
         loggingMessage("values reset, finding classes") 'DEBUG
         ' Find all classes with names that contain "exyHandler"
         Dim handlerClasses = AppDomain.CurrentDomain.GetAssemblies().SelectMany(Function(t) t.GetTypes()).Where(Function(t) t.IsClass AndAlso t.Name.Contains("exyHandler"))
         'If none found throw an error
-        If handlerClasses.Count < 1 Then errorHandler(New Exception("No handlers found. Please check your config files."), "Error loading handlers", "EXYEX", False)
+        If handlerClasses.Count < 1 Then errorHandler(New Exception("No handlers found. Please check your config files."), "Error loading handlers", "EXYEX", True)
 
         ' Now we need to get each class and ask it for the functions it'd like to add
         For Each handler In handlerClasses
@@ -66,7 +81,7 @@
                             Dim marker = curr.Split(";")(0)
                             i += 1 ' Move to next instruction
                             ' Check for an unexpected end
-                            If i > inst.Length - 1 Then errorHandler(New Exception("Unexpected end. You cannot define a marker as the last operation of an instruction set."), "Error defining marker '" & curr & "'", "line " & i + 1, False)
+                            If i > inst.Length - 1 Then errorHandler(New Exception("Unexpected end. You cannot define a marker as the last operation of an instruction set."), "Error defining marker '" & curr & "'", "line " & i + 1, True)
                             curr = inst(i) ' DO not need to change instruct index as the next instruction will be applied that index
                             ' Add to locations
                             If gotoLocations.ContainsKey(marker) Then
@@ -123,6 +138,7 @@
 
     ' Excecute code
     Shared Sub beginExcecution()
+        runningCode = True
         loggingMessage("excecution starting") ' DEBUG
         'todo check if instructions empty
         instructIndex = 0
@@ -130,34 +146,102 @@
             Dim params = instruct(instructIndex) ' params remember START at 1
             Dim func = params(0) ' inst
             loggingMessage(func & " index " & instructIndex)
-            'todo add built in
+            ' Now we replace with varibles if params ask for them (surrounded with [[varibleName]] - It is important that with var/set you do not use this)
+            ' pottentially v bad and slow code, may want to change if possible
+            For i = 0 To params.Count - 2
+                ' For each real param replace
+                For Each va In varibles
+                    loggingMessage("current var dump: " & "[[" & va.Key & "]]")
+                    If params(i + 1).Contains("[[" & va.Key & "]]") Then
+                        params(i + 1) = params(i + 1).Replace("[[" & va.Key & "]]", va.Value)
+                    End If
+
+                Next
+            Next
             If func = "" Then
                 ' Do nothing
-            ElseIf func = "goto" Then
+                'elifs here for built in fucntions
+            ElseIf func = "goto" Then ' !! ALWAYS FIRST TO HAVE FAST GOTO !!
+                ' syntax goto + marker
                 doGoto(params(1))
-                'elifs here for other commands
-            ElseIf exyconst.handlerList.ContainsKey(func) Then
-                'Another class can do this, pass it to them
-                exyconst.handlerClasses(exyconst.handlerList(func)).FunctionCall(func, params)
-            Else
-                'Not found
-                errorHandler(New Exception("No handler for function '" & func & "'"), "Runtime error", "instruction position " & instructIndex, False)
+            ElseIf func = "if" Then : doGoto(params(IIf(runtimeTemp = True, 1, 2))) ' syntax if + true marker + false marker
+            ElseIf func = "on error" OrElse func = "die" Then ' error handling in scripts
+                'syntax die + marker, also define varible with error message
+                If params(1) = "Default" Then : runningErrorHandling("enabled") = False ' If Default disable script handling
+                ElseIf params(1) = "Fatal On" OrElse params(1) = "Fatal Off" Then ' Modify fatal error handling
+                    warningHandler(New Exception("You have specified that you want your script to handle internal/fatal errors during excecution. EXY is not designed to run in this way, expect unexpected behaviour." & vbNewLine & vbNewLine _
+                        & "To disable these messages, use:" & vbNewLine & "warnings" & vbNewLine & "off"), "die/on error warning", "Instruction " & instructIndex, False)
+                    runningErrorHandling("evenFatal") = IIf(params(1) = "Fatal On", True, False)  ' Let scripts handle fatal errors by using this flag
+                    'Testing (see more, scroll right ->)
+                ElseIf params(1) = "TEST Normal" OrElse params(1) = "TEST Fatal" Then : errorHandler(New Exception("Test Exception"), "test", "test", IIf(params(1) = "TEST Fatal", True, False)) ' Testing error throwing, DO NOT REMOVE, useful for script writer
+                Else
+                    ' Set error handling flags
+                    runningErrorHandling("enabled") = True
+                    runningErrorHandling("gotoMarker") = params(1)
+                End If
+            ElseIf func = "warnings" Then : runningErrorHandling("warnings") = IIf(params(1) = "off", False, True) ' syntax warnings + on/off, disable warnings
+            ElseIf func = "var" OrElse func = "set" Then
+                ' Create/modify varible - syntax var + var name
+                If func = "var" Then : varMod(params(1), "Not set") : Else ' Set varible, else. ALSO WORKS TO DEFINE!
+                    ' Modify varible
+                    varMod(params(1), params(2))
+                End If
+            ElseIf exyconst.handlerList.ContainsKey(func) Then ' !! ALWAYS LAST !!
+                    'Another class can do this, pass it to them
+                    Try
+                        exyconst.handlerClasses(exyconst.handlerList(func)).FunctionCall(func, params)
+                    Catch ex As Exception
+                        If ex.GetType() = GetType(MissingMemberException) Then
+                            ' module maker messed up
+                            errorHandler(ex, "Runtime error", "Handler " & exyconst.handlerList(func) & vbCrLf & "!! Please contact the module developer regarding module errors !!", True)
+                        End If
+                        Throw ex
+                    End Try
+
+                Else
+                    'Not found
+                    errorHandler(New Exception("No handler for function '" & func & "'"), "Runtime error", "instruction position " & instructIndex, False)
             End If
             instructIndex += 1
 
         End While
+        runningCode = False
     End Sub
 
     Shared Sub doGoto(ByVal marker As String, Optional ByVal increment As Boolean = False)
         ' Perform a goto
-        If Not gotoLocations.ContainsKey(marker) Then errorHandler(New Exception("Marker '" & marker & "' not defined"), "Runtime goto error", "instruction position " & instructIndex, False)
+        If marker = "Continue" Then Exit Sub ' Continue, don't do goto
+        If Not gotoLocations.ContainsKey(marker) Then
+            errorHandler(New Exception("Marker '" & marker & "' not defined"), "Runtime goto error", "instruction position " & instructIndex, False)
+            Exit Sub
+        End If
         instructIndex = gotoLocations(marker) - IIf(increment, 0, 1) ' do goto, but take one as instructIndex will be incremented later
         loggingMessage("goto " & instructIndex + IIf(increment, 0, 1))
     End Sub
 
-    'Handle errors (non syntax)
-    Shared Sub errorHandler(ByVal err As Exception, ByVal ref As String, ByVal codeLine As Object, ByVal dumpVars As Boolean)
+    ' For making and changing varibles
+    Shared Sub varMod(ByVal Nm As String, ByVal val As Object)
+        If varibles.ContainsKey(Nm) Then
+            loggingMessage("var key " & Nm & " already exists, overwrite")
+            varibles(Nm) = val
+        Else
+            varibles.Add(Nm, val)
+        End If
+    End Sub
 
+
+    'Handle errors (non syntax)
+    Shared Sub errorHandler(ByVal err As Exception, ByVal ref As String, ByVal codeLine As Object, ByVal fatal As Boolean)
+        ' Will be done when running code
+        loggingMessage("ERROR HANDLER - err fatal: " & fatal.ToString() & vbNewLine &
+                       "script handling: " & runningErrorHandling("enabled").ToString)
+        If runningCode AndAlso IIf(runningErrorHandling("evenFatal"), True, Not fatal) AndAlso runningErrorHandling("enabled") Then
+            loggingMessage("Script will handle error " & err.ToString())
+            varMod("_ERROR", err.ToString)
+            varMod("_ERRORLOC", ref & " - " & codeLine)
+            doGoto(runningErrorHandling("gotoMarker"))
+            Exit Sub
+        End If
         ' Fatal error
         Console.BackgroundColor = ConsoleColor.Red
         Console.ForegroundColor = ConsoleColor.White
@@ -170,6 +254,7 @@
     End Sub
 
     Shared Sub warningHandler(ByVal err As Exception, ByVal ref As String, ByVal codeLine As Object, ByVal dumpVars As Boolean)
+        If runningCode AndAlso Not runningErrorHandling("warnings") Then Exit Sub ' Warning disables
         Console.BackgroundColor = ConsoleColor.DarkYellow
         Console.ForegroundColor = ConsoleColor.Black
         Console.WriteLine("WARNING")
